148a9c62cb4c867b25e3003fbac9e932eb059d28
Raphael23 <43172440+Raphael23@users.noreply.github.com>
2021-06-14 17:54:27 +0200

    fck frontend

--- src/WebUI/src/App.vue
+++ src/WebUI/src/App.vue
@@ -1,20 +1,28 @@
 <template>
-    <div id="app">
-        <Home msg="Hello world!" />
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-    import Home from './components/Home.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
-    export default {
-        name: 'app',
-        components: {
-            Home
-        }
-    };
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
-
--- src/WebUI/src/router/index.js
+++ src/WebUI/src/router/index.js
@@ -11,13 +11,13 @@ const routes = [
     component: Login,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/home",
+    name: "Home",
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (home.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "home" */ "../views/Home.vue"),
   },
 ];
 
