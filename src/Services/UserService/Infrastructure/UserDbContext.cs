9e96166aec8eb0112f6e3b1fefb15370bc431dc8
Raphael23 <43172440+Raphael23@users.noreply.github.com>
2021-06-15 23:44:15 +0200

    Added common layer again

---
+++ src/Services/UserService/Infrastructure/UserDbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA.Services.UserService.Infrastructure
+{
+    /// <summary>
+    /// Represents the <see cref="UserDbContext"/> class.
+    /// </summary>
+    public sealed class UserDbContext : UnitOfWork<UserDbContext>
+    {
+        /// <summary>
+        /// Gets and sets the users database set.
+        /// </summary>
+        public DbSet<UserAggregate> Users { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The user database context options.</param>
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The user database context options.</param>
+        /// <param name="mediator">The mediator.</param>
+        /// <param name="logger">The logger.</param>
+        public UserDbContext(DbContextOptions<UserDbContext> options, IMediator mediator, ILogger<UserDbContext> logger) : base(options, mediator, logger)
+        {
+
+        }
+
+        /// <summary>
+        /// Applies the entity configurations during the build of the model.
+        /// </summary>
+        /// <param name="modelBuilder">The model builder.</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserAggregateConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
